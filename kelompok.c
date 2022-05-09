c30adf701013b5aa9619b9f3360eef4c50fc9e30
RamaNuzwil <103976121+RamaNuzwil@users.noreply.github.com>
2022-05-09 10:29:30 +0700

    nuzwil

--- kelompok.c
+++ kelompok.c
@@ -4,5 +4,6 @@ main(){
   printf("Halo Nama saya Nurleli");
   printf("Halo Nama saya Alfin");
   printf("Halo Nama saya Dwi Ayu Apriliana Ningrum");
+  printf("Halo Nama saya nuzwil");
  return 0;
 }
